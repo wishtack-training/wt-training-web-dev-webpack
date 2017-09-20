32542ccd110dc3e423f78a20aa35d0635d9a1046
Younes JAAIDI <yjaaidi@gmail.com>
2017-09-20 17:13:41 +0200

    User list controller.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,69 @@
-/**
- *
- * (c) 2013-2017 Wishtack
- *
- * $Id: $
- */
 
-'use strict';
 
-import { User } from './user';
-import { UserStore } from './user-store';
+import {UserStore} from './user-store';
+import {User} from './user/user';
 
-const userStore = new UserStore();
-const user = new User('Foo', 'BAR');
+class UserListController {
 
-userStore.addUser(user);
+    constructor() {
+        this._userStore = new UserStore();
+    }
 
-console.log(userStore.getUserList());
+    run() {
+
+        const form = document.querySelector('form');
+
+        form.addEventListener('submit', (event) => {
+
+            event.preventDefault();
+
+            const user = new User(
+                form.querySelector('input[name="firstName"]').value,
+                form.querySelector('input[name="lastName"]').value
+            );
+
+            this._userStore.addUser(user);
+
+            this._renderUserList();
+
+        });
+
+        this._renderUserList();
+
+    }
+
+    _renderUserList() {
+
+        const userListElement = document.querySelector('ul');
+
+        userListElement.innerHTML = '';
+
+        this._userStore.getUserList()
+            .forEach((user) => {
+
+                const userElement = document.createElement('li');
+
+                userElement.innerHTML = `
+<span></span>
+<button>REMOVE</button>
+`;
+                userElement.querySelector('span').textContent = `${user.firstName} ${user.lastName}`;
+
+                userElement.querySelector('button')
+                    .addEventListener('click', () => this._removeUser(user));
+
+                userListElement.appendChild(userElement);
+
+            });
+
+
+    }
+
+    _removeUser(user) {
+        this._userStore.removeUser(user);
+        this._renderUserList();
+    }
+
+}
+
+new UserListController().run();
