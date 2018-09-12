b1af14d7dad52089f72b399dd7d322e0c5b0c750
Younes JAAIDI <yjaaidi@gmail.com>
2018-09-12 11:44:18 +0100

    Display user list.

--- user-store.spec.js
+++ user-store.spec.js
@@ -7,6 +7,15 @@
 
 'use strict';
 
+class User {
+
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
+
 const assertEquals = (expectedValue, value) => {
 
     if (value !== expectedValue) {
@@ -20,6 +29,26 @@ const assertEquals = (expectedValue, value) => {
 
 };
 
+class UserStore {
+
+    constructor() {
+        this._userList = [];
+    }
+
+    getUserList() {
+        return this._userList;
+    }
+
+    addUser(user) {
+        this._userList = [...this._userList, user];
+    }
+
+    removeUser(user) {
+        this._userList = this._userList.filter(_user => _user !== user);
+    }
+
+}
+
 const test = () => {
 
     const user1 = new User('Foo', 'BAR');
@@ -30,22 +59,22 @@ const test = () => {
 
     const userListEmpty = userStore.getUserList();
 
-    assertEquals(0, userListEmpty.length);
-
     userStore.addUser(user1);
     userStore.addUser(user2);
     userStore.addUser(user3);
 
     const userList1 = userStore.getUserList();
 
+    userStore.removeUser(user1);
+
+    const userList2 = userStore.getUserList();
+
+    assertEquals(0, userListEmpty.length);
+
     assertEquals(3, userList1.length);
     assertEquals(user1, userList1[0]);
     assertEquals(user2, userList1[1]);
     assertEquals(user3, userList1[2]);
-
-    userStore.removeUser(user1);
-
-    const userList2 = userStore.getUserList();
 
     assertEquals(2, userList2.length);
     assertEquals(user2, userList2[0]);
@@ -54,3 +83,35 @@ const test = () => {
 };
 
 test();
+
+const main = () => {
+
+    const userStore = new UserStore();
+
+    userStore.addUser(new User('Foo', 'BAR'));
+    userStore.addUser(new User('John', 'DOE'));
+
+    const userListContainer = document.querySelector('#wt-user-list');
+
+    for (const user of userStore.getUserList()) {
+
+        const userElement = document.createElement('div');
+
+        userElement.innerHTML = `
+<div>
+    <div><img src="" alt=""></div>
+    <div id="wt-user-name"></div>
+</div>
+`;
+
+        userElement.querySelector('img').src = `https://robohash.org/${encodeURIComponent(user.firstName)}`;
+
+        userElement.querySelector('#wt-user-name').textContent = `${user.firstName} ${user.lastName}`;
+
+        userListContainer.appendChild(userElement);
+
+    }
+
+};
+
+document.addEventListener('DOMContentLoaded', () => main());
