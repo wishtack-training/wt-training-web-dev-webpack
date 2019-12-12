eeb209bd1ebfecd6fb0548184dbddde7c0a128c6
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-12 21:14:24 +0100

    🛠 update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -10,36 +10,28 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 const srcPath = path.join(__dirname, 'src');
 const distPath = path.join(__dirname, 'dist');
 
 module.exports = {
-    entry: path.join(srcPath, 'index.ts'),
-    devServer: {
-        contentBase: distPath
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['es2015']
-                }
-            },
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.join(srcPath, 'index.html')
-        })
+  entry: path.join(srcPath, 'index.ts'),
+  devServer: {
+    contentBase: distPath
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(srcPath, 'index.html')
+    })
+  ]
 };
