e8221492058272eec3cc960e5a8d043b3323ef5a
Younes Jaaidi <yjaaidi@gmail.com>
2019-12-13 10:39:30 +0100

    🛠 create <mc-book-search> element

---
+++ src/elements/book-search.ts
@@ -0,0 +1,89 @@
+import { createQueryString } from '../helpers/create-query-string';
+
+class BookSearchElement extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <form id="searchForm">
+    <fieldset>
+      <legend>Search Criteria</legend>
+
+      <div>
+        <input
+          name="keywords"
+          placeholder="Book name or author..."
+          type="text"
+        />
+      </div>
+
+      <div>
+        <input name="freeOnly" type="checkbox" /><span>Free Books Only</span>
+      </div>
+
+      <select name="language">
+        <option value="en">🇬🇧 English</option>
+        <option value="fr">🇫🇷 French</option>
+      </select>
+    </fieldset>
+
+    <button type="submit">SEARCH</button>
+  </form>
+  <section data-role="searchResult"></section>`;
+
+    const form = this.querySelector('form');
+
+    form.addEventListener('submit', async event => {
+      event.preventDefault();
+
+      const criteria = {
+        keywords: (form.querySelector(
+          'input[name=keywords]'
+        ) as HTMLInputElement).value,
+        language: (form.querySelector(
+          'select[name=language]'
+        ) as HTMLInputElement).value,
+        freeOnly: (form.querySelector(
+          'input[name=freeOnly]'
+        ) as HTMLInputElement).checked
+      };
+
+      const queryParamMap = new Map();
+      queryParamMap.set('q', criteria.keywords);
+      queryParamMap.set('langRestrict', criteria.language);
+      queryParamMap.set('maxResults', 40);
+      if (criteria.freeOnly) {
+        queryParamMap.set('filter', 'free-ebooks');
+      }
+
+      const queryString = createQueryString(queryParamMap);
+
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?${queryString}`
+      );
+      const data = await response.json();
+
+      const bookList = data.items.map(item => {
+        return {
+          id: item.id,
+          title: item.volumeInfo.title,
+          pictureUri: item.volumeInfo.imageLinks.thumbnail
+        };
+      });
+
+      const bookElList = bookList.map(book => {
+        const bookEl = document.createElement('div');
+        bookEl.innerHTML = `
+          <h2></h2>
+          <img>
+          `;
+        bookEl.querySelector('h2').textContent = book.title;
+        bookEl.querySelector('img').src = book.pictureUri;
+        return bookEl;
+      });
+
+      this.querySelector('[data-role=searchResult]').innerHTML = '';
+      this.querySelector('[data-role=searchResult]').append(...bookElList);
+    });
+  }
+}
+
+customElements.define('mc-book-search', BookSearchElement);
